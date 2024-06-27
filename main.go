0369563d98072c535f7b820063ce3f2237f082f9
Shazia Naveeda <114483889+Ingeylas@users.noreply.github.com>
2024-06-28 04:03:35 +0700

    Update ENV

--- main.go
+++ main.go
@@ -3,12 +3,21 @@ package main
 import (
 	"TugasMID2/configs"
 	"TugasMID2/routes"
+	"os"
 )
 
 func main() {
 	configs.InitDB()
 
+	// e := routes.New()
+	// e.Logger.Fatal(e.Start(":8000"))
 	e := routes.New()
-	e.Logger.Fatal(e.Start(":8000"))
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	e.Logger.Fatal(e.Start("0.0.0.0:" + port))
 
 }
